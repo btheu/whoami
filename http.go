41c529ea9c4e28c63bf066e76a51772732d3fa9d
Benoit Theunissen <benoit.theunissen@gmail.com>
2018-06-29 11:55:34 +0200

    feat: display headers

--- http.go
+++ http.go
@@ -21,23 +21,33 @@ func main() {
     fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
     hostname, _ := os.Hostname()
     http.HandleFunc("/" + ctxt, func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(os.Stdout, "I'm %s \n", hostname)
-        fmt.Fprintf(os.Stdout, "Name: %s \n", name)
-        fmt.Fprintf(os.Stdout, "Desc: %s \n", desc)
-        
- 	      fmt.Fprintf(w, "I'm %s \n", hostname)
- 	      fmt.Fprintf(w, "Name: %s \n", name)
- 	      fmt.Fprintf(w, "Desc: %s \n", desc)
- 	      
- 	      fmt.Fprintf(os.Stdout, "==Env \n")
-        fmt.Fprintf(w, "==Env \n")
- 	      
- 	      for _, e := range os.Environ() {
+       fmt.Fprintf(os.Stdout, "I'm %s \n", hostname)
+       fmt.Fprintf(os.Stdout, "Name: %s \n", name)
+       fmt.Fprintf(os.Stdout, "Desc: %s \n", desc)
+       
+       fmt.Fprintf(w, "I'm %s \n", hostname)
+       fmt.Fprintf(w, "Name: %s \n", name)
+       fmt.Fprintf(w, "Desc: %s \n", desc)
+       
+       fmt.Fprintf(os.Stdout, "==Env \n")
+       fmt.Fprintf(w, "==Env \n")
+       
+       for _, e := range os.Environ() {
             pair := strings.Split(e, "=")
             fmt.Fprintf(os.Stdout, "%s = %s \n", pair[0], pair[1])
-            fmt.Fprintf(w, "%s = %s \n", pair[0], pair[1])
+            fmt.Fprintf(w,         "%s = %s \n", pair[0], pair[1])
         }
- 	      
+
+       fmt.Fprintf(os.Stdout, "==Headers \n")
+       fmt.Fprintf(w,         "==Headers \n")
+
+       for name, values := range r.Header {
+           for _, value := range values {
+             fmt.Fprintf(os.Stdout, "%s = %s \n", name, value)
+             fmt.Fprintf(w,         "%s = %s \n", name, value)
+           }
+        }
+
     })
 
 
